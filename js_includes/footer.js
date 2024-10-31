f1cafb07933ebc018f952adb99199ed24e390e22
dqlkambqlo-xx <devilman123_666@abv.bg>
2024-10-31 12:09:50 +0200

    Update footer.js

--- js_includes/footer.js
+++ js_includes/footer.js
@@ -3,5 +3,7 @@ document.getElementById("footer").innerHTML = `
   <p>Website design by <a href="#">dqlkambqlo-xx</a>.<br />
     © All your copyright information here.<br />
     If you encounter any issues with your purchase or have any questions, feel free to message me on Telegram at <strong>@dqlkambqlo_xx</strong>.</p>
+
+  <center><p>Сайтът се охранява от Делта Гард</p> <img src="https://www.deltaguard.org/wp-content/uploads/2020/10/Delta-Gard_logo-2020_final-150x150.png"></center>
 </div>
 `;
